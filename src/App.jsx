d7c560f1965c392cdf225aaa51711c72a2035063
mmaaxxii <maximilianoespeche@gmail.com>
2023-02-09 17:27:46 -0300

    Terminado Lista con ABM de tareas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import {TaskList} from "./components/TaskList";
+import { TaskList } from "./components/TaskList";
 import TaskForm from "./components/TaskForm";
 
 function App() {
   return (
-  <div>
-    <TaskForm/>
-    <TaskList/>
-  </div>
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
   );
 }
 export default App;
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -20,18 +20,22 @@ function TaskForm() {
   }
 
   return (
-    <div>
-        <form onSubmit={handleSubmit}>
+      <div className='max-w-md mx-auto'>
+        
+        <form onSubmit={handleSubmit} className="bg-slate-800 p-10 mb-4">
+            <h1 className='text-2xl font-bold text-white mb-3'>Crea tu tarea</h1>
             <input placeholder="Escribe tu tares"
             onChange={(e) => setTitle(e.target.value)}
             value={title}
+            className="bg-slate-300 p-3 w-full mb-2"
             autoFocus={true} />
             <textarea placeholder="Escribe la descripcion"
             onChange={(e) => setDescription(e.target.value)}
+            className="bg-slate-300 p-3 w-full mb-2"
             value={description} 
              />
-            <h1>a tu mama le gusta pro las nalgas</h1>
-            <button>
+            <button
+            className='bg-indigo-300 px-3 py-1 text-white'>
                 Guardar
             </button>
         </form>
